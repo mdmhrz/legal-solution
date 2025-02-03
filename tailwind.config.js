a563d3cd77574caf9949f6dd8efac47fa19f5195
razugit <razufreelance@gmail.com>
2025-02-04 04:19:56 +0600

    banner added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-tailwind.config = {
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {
-            colors: {
-                accent: '#B68C5A',
-                dark1: '#111111',
-                dark2: '#414040',
-                dark3: '#707070'
-            },
-            fontFamily: {
-                primary: ["Space Grotesk", "sans-serif"],
-                secondary: ["Poppins", "sans-serif"],
-            }
-        }
-    }
+        extend: {},
+    },
+    plugins: [],
 }
