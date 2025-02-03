ace312160fb661e9f1ba84a675939c8cf8bea149
razugit <razufreelance@gmail.com>
2025-02-04 01:53:16 +0600

    finally all setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
+tailwind.config = {
     theme: {
-        extend: {},
-    },
-    plugins: [],
+        extend: {
+            colors: {
+                accent: '#B68C5A',
+                dark1: '#111111',
+                dark2: '#414040',
+                dark3: '#707070'
+            },
+            fontFamily: {
+                primary: ["Space Grotesk", "sans-serif"],
+                secondary: ["Poppins", "sans-serif"],
+            }
+        }
+    }
 }
